d3579976f8fdc3c603cb290f9e5fe41b0ad45923
Taro <nathanon.chi@student.mahidol.edu>
2025-12-05 16:45:23 +0700

    Init React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tier List Maker</h1>
+      <p>Connected to Spring Boot backend!</p>
+    </div>
+  );
+}
+
+export default App;
